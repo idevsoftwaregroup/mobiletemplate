455bf8df3c4816643a9d09f388b7e8c666881397
i-dev Group <14952989+idevsoftwaregroup@users.noreply.github.com>
2026-07-29 23:19:32 +0330

    set roght align on the title of the index page

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,12 @@
 export default function Banner() {
   const description = "دست‌خط بستری برای انتشار دانش، تجربه و ایده‌های نو در دنیای فناوری، برنامه‌نویسی، امنیت، طراحی و خلاقیت دیجیتال است؛ جایی که تفکر انسان با تکنولوژی به زبان جدیدی تبدیل می‌شود."
-  const header = "دست خط"
+  const header = "دست خط";
+  const rightAlign = () => {
+    return "right-align"
+  };
   return (
     <article className="hero-banner primary-container round padding">
-      <h3>🚀 { header }</h3>
+      <h3 className={rightAlign()}>🚀 { header }</h3>
 
       <p>
         { description }
