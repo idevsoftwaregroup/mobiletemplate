87e78837e0a1ddfa81e7241b8169f6522a7bbcf1
i-dev Group <14952989+idevsoftwaregroup@users.noreply.github.com>
2026-07-29 23:33:10 +0330

    created some changes for styles ...

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,18 +1,25 @@
 export default function Banner() {
   const description = "دست‌خط بستری برای انتشار دانش، تجربه و ایده‌های نو در دنیای فناوری، برنامه‌نویسی، امنیت، طراحی و خلاقیت دیجیتال است؛ جایی که تفکر انسان با تکنولوژی به زبان جدیدی تبدیل می‌شود."
   const header = "دست خط";
-  const rightAlign = () => {
-    return "right-align"
-  };
+// some styles
+  const style = {
+    rightAlign: "right-align",
+    leftAlign: "left-align",
+    smallHeader: "small",
+    mediumHeader: "medium",
+    largeHeader: "large"
+  }
+
+
   return (
     <article className="hero-banner primary-container round padding">
-      <h3 className={rightAlign()}>{ header } 🚀</h3>
+      <h3 className={style.rightAlign}>{ header } 🚀</h3>
 
       <p>
         { description }
       </p>
 
-      <nav>
+      <nav className={style.rightAlign}>
         <button className="primary slow-ripple">
           <i>rocket_launch</i>
           <span>راه اندازی اولیه</span>
