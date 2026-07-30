996775cb60b570f610bda7dc32963ada67bdebfe
i-dev Group <14952989+idevsoftwaregroup@users.noreply.github.com>
2026-07-30 04:26:42 +0330

    some layouts are here and changed ...

--- src/components/LayoutComponent.tsx
+++ src/components/LayoutComponent.tsx
@@ -9,19 +9,18 @@ export default function LayoutComponent() {
       <Navbar />
 
       <main className="responsive padding">
-        <div className="no-space"></div>
-          <div className="field label suffix no-border round extra custom-input ">
-              <input type="text" />
-              <label >جستجو کن</label>
-              <i>search</i>
-          </div>
 
-        <div className="extra-large-space large-margin"></div>
-
+        <div className="responsive">
+            <div className="no-space"></div>
+            <div className="field label suffix no-border round extra custom-input ">
+                <input type="text" />
+                <label >جستجو کن</label>
+                <i>search</i>
+            </div>
+        </div>
+        <div className="responsive">
           <Banner />
-
-        <div className="extra-large-space"></div>
-
+        </div>
 
       </main>
     </>
